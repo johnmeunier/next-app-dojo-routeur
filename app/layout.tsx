418e8608782c63a8a3fe0bbadfb9f36ba6e40929
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2023-12-14 11:24:35 +0100

    prepa chap 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import '@/app/ui/global.css';
-import React, { PropsWithChildren } from 'react';
+import { inter } from '@/app/ui/fonts';
 
-const RootLayout = ({ children }: PropsWithChildren) => (
-  <html lang="en">
-    <body>{children}</body>
-  </html>
-);
-
-export default RootLayout;
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
